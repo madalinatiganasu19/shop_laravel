958a5b0d446dc74038a8bab7e402b2bd8e289aee
Laurentiu Dodoloi <dodoloilaurentiu@gmail.com>
2018-09-06 13:05:30 +0300

    #12: Index http://pm.5psolutions.net/public/index.php?path_info=projects/training-madalina-tiganasu/tasks/12 resolve storage path

--- resources/views/pages/cart.blade.php
+++ resources/views/pages/cart.blade.php
@@ -2,5 +2,24 @@
 
 @section('content')
 
+    <table>
+        @foreach($products as $product)
+            <tr>
+                <td><img class="img-thumbnail" src="{{\Illuminate\Support\Facades\Storage::url(('public/images/' . $product->image))}}"></td>
+                <td>&nbsp;&nbsp;&nbsp;</td>
+                <td>
+                    <p class="lead">{{ $product->title }}</p>
+                    <p>{{ $product->description }}</p>
+                    <p class="lead">
+                        {{ __("$") }}
+                        {{ $product->price }}
+                    </p>
+                </td>
+                <td>&nbsp;&nbsp;&nbsp;</td>
+                <td><a class="btn btn-dark" href="?id={{ $product->id }}">{{ __("Remove from Cart") }}</a></td>
+            </tr>
+        @endforeach
+    </table>
+
 @endsection
 
