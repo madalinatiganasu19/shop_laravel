588b1693c460668124ea2f9bd2a0a0c91c8241d5
Laurentiu Dodoloi <dodoloilaurentiu@gmail.com>
2018-09-06 11:22:44 +0300

    #11: Migrations, eloquent models, relations http://pm.5psolutions.net/public/index.php?path_info=projects/training-madalina-tiganasu/tasks/11 rename resource routes

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
 
@@ -15,6 +16,12 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         Schema::defaultStringLength(191);
+
+        Route::resourceVerbs([
+            'create' => 'product',
+            'edit' => 'product',
+            '' => '',
+        ]);
     }
 
     /**
