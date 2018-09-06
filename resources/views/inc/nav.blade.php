2bce306cbd282fc04032e874cfa155346046ada1
Laurentiu Dodoloi <dodoloilaurentiu@gmail.com>
2018-09-06 11:23:47 +0300

    #11: Migrations, eloquent models, relations http://pm.5psolutions.net/public/index.php?path_info=projects/training-madalina-tiganasu/tasks/11 add routing

--- resources/views/inc/nav.blade.php
+++ resources/views/inc/nav.blade.php
@@ -1,7 +1,7 @@
 <nav class="navbar navbar-expand-md navbar-dark bg-dark navbar-laravel">
     <div class="container">
-        <a class="navbar-brand" href="{{ route('/') }}">
-            {{ config('app.name', 'Shop Laravel') }}
+        <a class="navbar-brand" href="">
+            {{ config('app.name', 'Pastry Shop') }}
         </a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
             <span class="navbar-toggler-icon"></span>
@@ -10,7 +10,7 @@
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <!-- Left Side Of Navbar -->
             <ul class="navbar-nav mr-auto">
-                <li class="nav-item"><a class="nav-link" href="{{ route('products.index') }}">{{ __('Home') }}</a></li>
+                <li class="nav-item"><a class="nav-link" href="{{ route('index') }}">{{ __('Home') }}</a></li>
                 <li class="nav-item"><a class="nav-link" href="">{{ __('Shopping Cart') }}</a></li>
             </ul>
 
@@ -24,7 +24,7 @@
                     <li class="nav-item">
                         <a class="nav-link" href="">{{ __('Register') }}</a>
                     </li>
-                
+
             </ul>
         </div>
     </div>
--- routes/web.php
+++ routes/web.php
@@ -13,9 +13,9 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function(){
+Route::get('/welcome', function(){
     return view('pages.welcome');
-})->name('/');
+})->name('welcome');
 
-Route::resource('products', 'ProductsController');
+Route::resource('/', 'ProductsController');
 
