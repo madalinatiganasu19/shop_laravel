85481042742f15e3a6a0bee3da870ce1af085156
Laurentiu Dodoloi <dodoloilaurentiu@gmail.com>
2018-09-06 16:23:48 +0300

    #13: Cart http://pm.5psolutions.net/public/index.php?path_info=projects/training-madalina-tiganasu/tasks/13 crrate checkout form

--- resources/views/pages/cart.blade.php
+++ resources/views/pages/cart.blade.php
@@ -21,5 +21,21 @@
         @endforeach
     </table>
 
+    <form class="my-4" method="POST">
+        @csrf
+        <div class="form-group">
+            <input class="form-control" type="text" name="name" placeholder="{{__('Name')}}" value="">
+        </div>
+        <div class="form-group">
+            <input class="form-control" type="text" name="email" placeholder="{{__('Email')}}" value="">
+        </div>
+        <div class="form-group">
+            <textarea class="form-control" rows="5" type="comments" name="name" placeholder="{{__('Comments')}}"></textarea>
+        </div>
+        <div class="form-group text-right">
+            <input class="btn btn-dark" type="submit" name="checkout" value="{{__('Checkout')}}">
+        </div>
+    </form>
+
 @endsection
 
