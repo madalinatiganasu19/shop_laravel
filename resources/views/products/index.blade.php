b4fbc0d004da461de2d49708d65bdd505f24b464
Laurentiu Dodoloi <dodoloilaurentiu@gmail.com>
2018-09-07 16:42:03 +0300

    #15: Products http://pm.5psolutions.net/public/index.php?path_info=projects/training-madalina-tiganasu/tasks/15 display products

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -2,4 +2,27 @@
 
 @section('content')
 
+    <table>
+        @foreach ($products as $product)
+            <tr>
+                <td><img class="img-thumbnail" src="{{\Illuminate\Support\Facades\Storage::url(('images/' . $product->image))}}"></td>
+                <td>&nbsp;&nbsp;&nbsp;</td>
+                <td>
+                    <p class="lead">{{ $product->title }}</p>
+                    <p>{{ $product->description }}</p>
+                    <p class="lead">
+                        {{ __("$") }}
+                        {{ $product->price }}
+                    </p>
+                </td>
+                <td>&nbsp;&nbsp;&nbsp;</td>
+                <td>
+                    <a class="btn btn-success my-1" href="{{ route('product') }}?id={{ $product->id }}">{{ __("Update") }}</a>
+                    <a class="btn btn-danger my-1" href="?id={{ $product->id }}">{{ __("Delete") }}</a>
+                </td>
+            </tr>
+        @endforeach
+    </table>
+
+
 @endsection
