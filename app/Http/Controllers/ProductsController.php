90c15aa71bda5a708bbddb2ca5789fd4411594bb
Laurentiu Dodoloi <dodoloilaurentiu@gmail.com>
2018-09-07 16:41:45 +0300

    #15: Products http://pm.5psolutions.net/public/index.php?path_info=projects/training-madalina-tiganasu/tasks/15 fetch products

--- app/Http/Controllers/ProductsController.php
+++ app/Http/Controllers/ProductsController.php
@@ -2,11 +2,18 @@
 
 namespace App\Http\Controllers;
 
+use App\Product;
 use Illuminate\Http\Request;
 
 class ProductsController extends Controller
 {
-    public function index() {
-        return view('products.index');
+    public function index(Product $product) {
+
+        $query = Product::query();
+        if (session('cart')) {
+            $query->whereNotIn(($product)->getKeyName(), session('cart'));
+        }
+
+        return view('products.index')->with('products', $query->get());
     }
 }
