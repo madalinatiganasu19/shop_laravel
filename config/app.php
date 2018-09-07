d4f6cfde1c0e81ccfc202ef1e363b13fab7d3a14
Laurentiu Dodoloi <dodoloilaurentiu@gmail.com>
2018-09-07 14:33:35 +0300

    #14: Login http://pm.5psolutions.net/public/index.php?path_info=projects/training-madalina-tiganasu/tasks/14 add admin credentials

--- config/app.php
+++ config/app.php
@@ -13,6 +13,33 @@ return [
     |
     */
 
+    'email' => env('ADMIN_EMAIL'),
+
+    /*
+    |--------------------------------------------------------------------------
+    | Application Name
+    |--------------------------------------------------------------------------
+    |
+    | This value is the name of your application. This value is used when the
+    | framework needs to place the application's name in a notification or
+    | any other location as required by the application or its packages.
+    |
+    */
+
+    'password' => env('ADMIN_PASSWORD'),
+
+
+    /*
+    |--------------------------------------------------------------------------
+    | Application Name
+    |--------------------------------------------------------------------------
+    |
+    | This value is the name of your application. This value is used when the
+    | framework needs to place the application's name in a notification or
+    | any other location as required by the application or its packages.
+    |
+    */
+
     'name' => env('APP_NAME', 'Shop Laravel'),
 
     /*
