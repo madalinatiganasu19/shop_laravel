9fa600c07d6bf9cfaee85974602466808b66a91d
Laurentiu Dodoloi <dodoloilaurentiu@gmail.com>
2018-09-10 15:11:47 +0300

    #15: Products http://pm.5psolutions.net/public/index.php?path_info=projects/training-madalina-tiganasu/tasks/15 populate form for create and edit

--- resources/views/products/create.blade.php
+++ resources/views/products/create.blade.php
@@ -15,20 +15,24 @@
     <form class="my-4" method="POST" enctype="multipart/form-data">
         @csrf
         <div class="form-group">
-            <input class="form-control" type="text" name="title" placeholder="{{__('Title')}}" value="{{ old('title', $product->title) }}">
+            <input class="form-control" type="text" name="title" placeholder="{{__('Title')}}" value="{{ request('id') ? old('title', $product->title) : old('title') }}">
         </div>
 
         <div class="form-group">
-            <textarea class="form-control" rows="5" type="text" name="description" placeholder="{{__('Description')}}">{{ old('description', $product->description) }}</textarea>
+            <textarea class="form-control" rows="5" type="text" name="description" placeholder="{{__('Description')}}">
+                {{ request('id') ? old('description', $product->description) : old('description') }}
+            </textarea>
         </div>
 
+
         <div class="form-group">
-            <input class="form-control" type="text" name="price" placeholder="{{__('Price')}}" value="{{ old('price', $product->price) }}">
+            <input class="form-control" type="text" name="price" placeholder="{{__('Price')}}" value="{{ request('id') ? old('price', $product->price) : old('price') }}">
         </div>
+
 
         <div class="form-group">
             <div class="custom-file">
-                <input type="file" name="image" class="custom-file-input" id="validatedCustomFile" value="{{ old('image', $product->image) }}">
+                <input type="file" name="image" class="custom-file-input" id="validatedCustomFile">
                 <label class="custom-file-label" for="validatedCustomFile">Choose file...</label>
                 <div></div>
             </div>
