c00e8c8cb35d7564d4302ef7d7bfbf941c87d977
Laurentiu Dodoloi <dodoloilaurentiu@gmail.com>
2018-09-11 16:59:04 +0300

    #17: Orders http://pm.5psolutions.net/public/index.php?path_info=projects/88/tasks/17

--- resources/views/products/product.blade.php
+++ resources/views/products/product.blade.php
@@ -15,16 +15,15 @@
     <form class="my-4" method="POST" enctype="multipart/form-data">
         @csrf
         <div class="form-group">
-            <input class="form-control" type="text" name="title" placeholder="{{__('Title')}}" value="{{ request('id') ? request('title', $product->title) : request('title') }}">
+            <input class="form-control" type="text" name="title" placeholder="{{__('Title')}}" value="{{ request('id') ? old('title', $product->title) : old('title') }}">
         </div>
 
         <div class="form-group">
-            <textarea class="form-control" rows="5" type="text" name="description" placeholder="{{__('Description')}}">{{ request('id') ? request('description', $product->description) : request('description') }}</textarea>
+            <textarea class="form-control" rows="5" type="text" name="description" placeholder="{{__('Description')}}">{{ request('id') ? old('description', $product->description) : old('description') }}</textarea>
         </div>
 
-
         <div class="form-group">
-            <input class="form-control" type="text" name="price" placeholder="{{__('Price')}}" value="{{ request('id') ? request('price', $product->price) : request('price') }}">
+            <input class="form-control" type="text" name="price" placeholder="{{__('Price')}}" value="{{ request('id') ? old('price', $product->price) : old('price') }}">
         </div>
 
 
