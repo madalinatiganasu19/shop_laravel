aac22f2b32f04b03354fa7d4c6afa5d6be925523
Laurentiu Dodoloi <dodoloilaurentiu@gmail.com>
2018-09-26 13:31:43 +0300

    #18: Documentation and starter http://pm.5psolutions.net/public/index.php?path_info=projects/training-madalina-tiganasu/tasks/18 optimize renderErrors function | declare all variables | add spacing and indentation | remove common code

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -16,14 +16,14 @@
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
 
     <!-- Custom JS script -->
+    <script src="{{ asset('js/spa.js') }}" defer></script>
     <script type="text/javascript">
 
         function getUrlVars() {
-            vars = [];
-            hashes = window.location.href.slice(window.location.href.indexOf('?') + 1).split('&');
-            for(i = 0; i < hashes.length; i++)
-            {
-                hash = hashes[i].split('=');
+            var vars = [];
+            var hashes = window.location.href.slice(window.location.href.indexOf('?') + 1).split('&');
+            for (i = 0; i < hashes.length; i++) {
+                var hash = hashes[i].split('=');
                 vars.push(hash[0]);
                 vars[hash[0]] = hash[1];
             }
@@ -48,98 +48,102 @@
             });
 
             function renderList(products, location) {
-                html = [];
+                var html = [];
 
                 $.each(products, function (key, product) {
                     html += [
                         '<tr>',
-                        '<td><img class="img-thumbnail" src="{{\Illuminate\Support\Facades\Storage::url('images/')}}'+ product.image+'"></td>',
-                        '<td>&nbsp;&nbsp;&nbsp;</td>',
-                        '<td>',
-                        '<p class="lead">' + product.title + '</p>',
-                        '<p>' + product.description + '</p>',
-                        '<p class="lead"> {{ __('$') }}' + product.price + '</p>',
-                        '</td>',
-                        '<td>&nbsp;&nbsp;&nbsp;</td>'
+                            '<td>',
+                                '<img class="img-thumbnail" src="{{\Illuminate\Support\Facades\Storage::url('images/')}}'+ product.image+'">',
+                            '</td>',
+                            '<td>&nbsp;&nbsp;&nbsp;</td>',
+                            '<td>',
+                                '<p class="lead">' + product.title + '</p>',
+                                '<p>' + product.description + '</p>',
+                                '<p class="lead"> {{ __('$') }}' + product.price + '</p>',
+                            '</td>',
+                            '<td>&nbsp;&nbsp;&nbsp;</td>'
                     ].join('');
 
                     if (location === '') {
                         html += [
-                            '<td class="text-center"><a href="#?id='+ product.id +'" class="btn btn-sm btn-dark">{{__('Add to Cart')}}</a></td>',
-                            '</tr>'
+                            '<td class="text-center">',
+                                '<a href="#?id='+ product.id +'" class="btn btn-sm btn-dark">{{__('Add to Cart')}}</a>',
+                            '</td>'
                         ].join('');
                     } else if (location === 'cart') {
                         html += [
-                            '<td class="text-center"><a href="#cart?id='+ product.id +'" class="btn btn-sm btn-dark">{{__('Remove from Cart')}}</a></td>',
-                            '</tr>'
+                            '<td class="text-center">',
+                                '<a href="#cart?id='+ product.id +'" class="btn btn-sm btn-dark">{{__('Remove from Cart')}}</a>',
+                            '</td>'
                         ].join('');
                     } else if (location === 'products') {
                         html += [
-                            '<td class="text-center">' +
-                                '<a href="#product?id='+ product.id +'" class="btn btn-sm btn-success my-1">{{__('Update')}}</a>' +
-                                '<a href="#products?id='+ product.id +'" class="btn btn-sm btn-danger my-1">{{__('Delete')}}</a>' +
-                            '</td>',
-                            '</tr>'
+                            '<td class="text-center">',
+                                '<a href="#product?id='+ product.id +'" class="btn btn-sm btn-success my-1">{{__('Update')}}</a>',
+                                '<a href="#products?id='+ product.id +'" class="btn btn-sm btn-danger my-1">{{__('Delete')}}</a>',
+                            '</td>'
                         ].join('');
                     }
 
+                    html += '</tr>';
                 });
 
                 return html;
             }
-            function renderOrders(orders, location) {
-                html = [
+            function renderOrders(orders) {
+                var html = [
                     '<tr class="bg-dark text-white text-center">',
-                    '<th><p class="lead">{{__('NAME')}}</p></th>',
-                    '<th>&nbsp;&nbsp;&nbsp;</th>',
-                    '<th><p class="lead">{{__('EMAIL')}}</p></th>',
-                    '<th>&nbsp;&nbsp;&nbsp;</th>',
-                    '<th><p class="lead">{{__('TOTAL')}}</p></th>',
-                    '<th></th>',
-                    '<th></th>',
+                        '<th>',
+                            '<p class="lead">{{__('NAME')}}</p>',
+                        '</th>',
+                        '<th>&nbsp;&nbsp;&nbsp;</th>',
+                        '<th>',
+                            '<p class="lead">{{__('EMAIL')}}</p>',
+                        '</th>',
+                        '<th>&nbsp;&nbsp;&nbsp;</th>',
+                        '<th>',
+                            '<p class="lead">{{__('TOTAL')}}</p>',
+                        '</th>',
+                        '<th></th>',
+                        '<th></th>',
                     '</tr>'
-                ];
+                ].join('');
 
                 $.each(orders, function (key, order) {
                     html += [
                         '<tr>',
-                        '<td><p >' + order.name + '</p></td>',
-                        '<td>&nbsp;&nbsp;&nbsp;</td>',
-                        '<td><p>' + order.email + '</p></td>',
-                        '<td>&nbsp;&nbsp;&nbsp;</td>',
-                        '<td><p > {{ __("$") }}' + order.total + '</p></td>',
-                        '<td>&nbsp;&nbsp;&nbsp;</td>'
+                            '<td>',
+                                '<p >' + order.name + '</p>',
+                            '</td>',
+                            '<td>&nbsp;&nbsp;&nbsp;</td>',
+                            '<td>',
+                                '<p>' + order.email + '</p>',
+                            '</td>',
+                            '<td>&nbsp;&nbsp;&nbsp;</td>',
+                            '<td>',
+                                '<p > {{ __("$") }}' + order.total + '</p>',
+                            '</td>',
+                            '<td>&nbsp;&nbsp;&nbsp;</td>',
+                            '<td class="text-center">',
+                                '<a href="#order?id='+ order.id +'" class="btn btn-sm btn-dark">{{__('View Order')}}</a>',
+                            '</td>',
+                        '</tr>'
                     ].join('');
 
-                    if (location === 'orders') {
-                        html += [
-                            '<td class="text-center"><a href="#order?id='+ order.id +'" class="btn btn-sm btn-dark">{{__('View Order')}}</a></td>',
-                            '</tr>'
-                        ].join('');
-                    } else if (location === 'order') {
-                        html += [
-                            '</tr>'
-                        ].join('');
-                    }
+
                 });
 
                 return html;
             }
-            function renderListErrors(response) {
-                errors = response.responseJSON.errors;
-                error_list = [];
+            function renderErrors(response) {
+                var errors = response.responseJSON.errors;
+                var error_list = "";
 
-                if (errors.hasOwnProperty('title')) {
-                    error_list += ['<p>' + errors.title + '</p>'].join();
-                }
-                if (errors.hasOwnProperty('description')) {
-                    error_list += ['<p>' + errors.description + '</p>'].join();
-                }
-                if (errors.hasOwnProperty('price')) {
-                    error_list += ['<p>' + errors.price + '</p>'].join();
-                }
-                if (errors.hasOwnProperty('image')) {
-                    error_list += ['<p>' + errors.image + '</p>'].join();
+                for (var name in errors) {
+                    if (errors.hasOwnProperty(name)) {
+                        error_list += '<p>' + errors[name] + '</p>';
+                    }
                 }
 
                 $('.alert').html(error_list);
@@ -149,7 +153,7 @@
             $('.checkout-form').submit(function(event) {
                 event.preventDefault();
 
-                formData = $('.checkout-form').serialize();
+                var formData = $('.checkout-form').serialize();
 
                 $.ajax('/cart',  {
                     dataType: 'json',
@@ -167,20 +171,8 @@
                     },
                     error: function (response) {
 
-                        errors = response.responseJSON.errors;
-                        error_list = [];
-
-                        if (errors.hasOwnProperty('name')) {
-                            error_list += ['<p>' + errors.name + '</p>'].join();
-                        }
-                        if (errors.hasOwnProperty('email')) {
-                            error_list += ['<p>' + errors.email + '</p>'].join();
-                        }
-                        if (errors.hasOwnProperty('comments')) {
-                            error_list += ['<p>' + errors.comments + '</p>'].join();
-                        }
-
-                        $('.alert').show().html(error_list);
+                        renderErrors(response);
+                        $('.alert').show();
                     }
                 });
 
@@ -189,7 +181,7 @@
             $('.add-product').submit(function(event) {
                 event.preventDefault();
 
-                productData = new FormData(this);
+                var productData = new FormData(this);
 
                 $.ajax('/product' + (getUrlVars().id ? '?id='+getUrlVars().id : ''),  {
                     dataType: 'json',
@@ -202,8 +194,8 @@
                     },
 
                     error: function (response) {
+                        renderErrors(response);
                         $('.alert').show();
-                        renderListErrors(response);
                     }
                 });
 
@@ -212,7 +204,7 @@
             $('.login-form').submit(function(event) {
                 event.preventDefault();
 
-                loginData = $('.login-form').serialize();
+                var loginData = $('.login-form').serialize();
 
                 $.ajax('/login',  {
                     dataType: 'json',
@@ -231,18 +223,8 @@
                         }
                     },
                     error: function (response) {
-
-                        errors = response.responseJSON.errors;
-                        error_list = [];
-
-                        if (errors.hasOwnProperty('email')) {
-                            error_list += ['<p>' + errors.email + '</p>'].join();
-                        }
-                        if (errors.hasOwnProperty('password')) {
-                            error_list += ['<p>' + errors.password + '</p>'].join();
-                        }
-
-                        $('.alert').show().html(error_list);
+                        renderErrors(response);
+                        $('.alert').show();
                     }
                 });
 
@@ -262,6 +244,7 @@
 
                 // First hide all the pages
                 $('.page').hide();
+                $('.alert').hide();
 
                 switch (window.location.hash) {
 
@@ -275,7 +258,6 @@
                                 success: function (response) {
                                     // Render the products in the cart list
                                     $('.cart .list').html(renderList(response, 'cart'));
-                                    $('.alert').hide();
                                 }
                             });
 
@@ -297,13 +279,6 @@
                     case '#login':
                         // Show the login page
                         $('.login').show();
-                        //
-                        $.ajax('/login', {
-                            dataType: 'json',
-                            success: function (response) {
-                                $('.alert').hide();
-                            }
-                        });
 
                         break;
 
@@ -324,8 +299,6 @@
                         break;
 
                     case '#products?id='+getUrlVars().id:
-                        //take product id
-                        id = getUrlVars().id;
                         // Delete product
                         $.ajax('/products', {
                             dataType: 'json',
@@ -345,7 +318,6 @@
 
                         $.ajax('/product', {
                             success: function (response) {
-                                $('.alert').hide();
 
                                 $('.title').val('');
                                 $('.description').val('');
@@ -370,7 +342,6 @@
                             dataType: 'json',
                             data: {'id': getUrlVars().id},
                             success: function (response) {
-                                $('.alert').hide();
 
                                 $('.title').val(response.title);
                                 $('.description').val(response.description);
@@ -392,7 +363,7 @@
                             dataType: 'json',
                             success: function (response) {
                                 // Render the orders in the orders list
-                                $('.orders .list').html(renderOrders(response, 'orders'));
+                                $('.orders .list').html(renderOrders(response));
                             },
                             error: function (response) {
                                 location.href = '#login';
@@ -403,7 +374,6 @@
                     case '#order?id='+getUrlVars().id:
                         // Show the order page
                         $('.order').show();
-                        id = getUrlVars().id;
                         // Load the order products from the server
                         $.ajax('/order', {
                             dataType: 'json',
